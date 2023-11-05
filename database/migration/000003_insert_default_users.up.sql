eed722a96e6f8a2be174b2f42ccd07460b9c5cb1
Rahmat Ramadhan <rahmatramadhan.ti.poliwangi@gmail.com>
2023-11-05 13:10:12 +0700

    fix: database migration

---
+++ database/migration/000003_insert_default_users.up.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    `users` (
+        `email`,
+        `phone`,
+        `name`,
+        `role`,
+        `password`
+    )
+VALUES
+    (
+        'admin@admin.com',
+        '08734123124213',
+        'Admin',
+        2,
+        '$2a$10$LVeec3QGFBhZgoUB0mAnYuf1r.B113afseQIWNSLAQR9HIkGyF8Vy' -- value: asdasd
+    );
